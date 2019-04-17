28e2b94aaa722a2c5579298e0edaa61fd54efa2e
wfreshour <39931412+wfreshour@users.noreply.github.com>
2019-04-17 14:45:29 -0500

    Update Form1.vb
    Here You Go!

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub btnPressMe_Click(sender As Object, e As EventArgs) Handles btnPressMe.Click
-        lblHelloWorld.Text = "Hello World"
+        lblHelloWorld.Text = "Hallo Welt"
     End Sub
 End Class
